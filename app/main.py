3cceef46e095af58edf724f7d5f4ed161820f317
HesoyamFil <126004332+HesoyamFil@users.noreply.github.com>
2023-04-11 17:48:23 +0300

    fix main and get_history
    main added getenv
    get_history removed unused-import and changed func limit()

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 """main"""
-from app.get_history.get_history import pars_history
+import os
+from get_history.get_history import limit, pars_history
 
 
 BASE_CURRENCIES='usdt'
-STORAGE = r'C:PATH'
+STORAGE = os.getenv('HISTORY_PATH')
 
 if __name__ == "__main__":
     pars_history(BASE_CURRENCIES, STORAGE)
+    limit()
