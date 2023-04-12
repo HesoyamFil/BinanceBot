ad3f07865bd8e2f8d37f7b58e1f86dd969c7bff9
HesoyamFil <126004332+HesoyamFil@users.noreply.github.com>
2023-04-12 13:48:22 +0300

    add freeze
    add freeze > requirements.txt

--- app/main.py
+++ app/main.py
@@ -7,5 +7,5 @@ BASE_CURRENCIES='usdt'
 STORAGE = os.getenv('HISTORY_PATH')
 
 if __name__ == "__main__":
-    pars_history(BASE_CURRENCIES, STORAGE)
+    #pars_history(BASE_CURRENCIES, STORAGE)
     limit()
